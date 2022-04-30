a7af1ce6c08c7e6822388dd6f1ee6f15432e09d8
liliya <1752181917@qq.com>
2022-04-30 10:30:46 +0800

    修改了一些名字

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@
 <script setup lang="ts">
 useHead({
   meta: [
-    { property: 'og:title', content: 'Anthony Fu' },
-    { property: 'og:image', content: 'https://antfu.me/avatar.png' },
-    { name: 'description', content: 'Anthony Fu\'s Portfolio' },
-    { name: 'twitter:card', content: 'summary' },
-    { name: 'twitter:creator', content: '@antfu7' },
+    { property: 'og:title', content: 'Liliya' },
+    { property: 'og:image', content: 'https://avatars.githubusercontent.com/u/50412437?s=400&u=308833608ef36baa60f51d695d18fc6159cfc6c5&v=4' },
+    { name: 'description', content: 'Liliya\'s Portfolio' },
+    // { name: 'twitter:card', content: 'summary' },
+    // { name: 'twitter:creator', content: '@antfu7' },
   ],
 })
 </script>
