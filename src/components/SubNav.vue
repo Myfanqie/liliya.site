259f16ce0a592c1ed24dff4ca4f3d0760e80c867
liliya <1752181917@qq.com>
2022-05-02 11:46:58 +0800

    移除English Only

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -9,10 +9,10 @@ const route = useRoute()
 
 <template>
   <div class="prose m-auto mb-8 select-none">
-    <button flex="~ gap1" items-center mb2 op30 text-xs @click="englishOnly = !englishOnly">
+    <!-- <button flex="~ gap1" items-center mb2 op30 text-xs @click="englishOnly = !englishOnly">
       <div :i="englishOnly ? 'carbon-checkbox-checked' : 'carbon-checkbox'" />
       English Only
-    </button>
+    </button> -->
 
     <div mb-0 flex="~ gap-3" text-3xl>
       <router-link to="/posts" class="!border-none !font-400" :class="route.path === '/posts' ? '' : inactiveStyle">
